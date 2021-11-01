19b93275c85613daf474a49e9074721c61a8641e
Morgan Quigley <morgan@osrfoundation.org>
2021-11-01 17:27:16 +0800

    first attempt at a deploy script
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

---
+++ scripts/deploy-web.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone ssh://git@github.com/codebot/traffic_editor_iii temp-deploy-checkout
+cd temp-deploy-checkout
+git checkout --orphan gh-pages
+git reset
+scripts/build-web.sh
+git add web
+cp web/root_index.html ./index.html
+git add index.html
+git commit -a -m "publish to github pages"
+git push origin gh-pages --force
