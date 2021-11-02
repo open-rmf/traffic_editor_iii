f800e7b6f3ea32a49c32fa08df8972288b48f822
Morgan Quigley <morgan@osrfoundation.org>
2021-11-02 16:38:47 +0800

    optimize WASM binary for size and use LTO
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Alternatively, if you already have a Rust installation managed by `rustup`, you 
 
 Next, install some dependencies and the source:
 ```
-sudo apt install lld libxcb-shape0-dev libxcb-xfixes0-dev
+sudo apt install lld libxcb-shape0-dev libxcb-xfixes0-dev binaryen
 git clone ssh://git@github.com/open-rmf/traffic_editor_iii
 cd traffic_editor_iii
 cargo build --release
--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -o verbose
 set -o errexit
+export CARGO_PROFILE_RELEASE_LTO=true
+export CARGO_PROFILE_RELEASE_OPT_LEVEL=z
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen --target web --out-dir web target/wasm32-unknown-unknown/release/libtraffic_editor_iii.wasm
+cd web
+wasm-opt -Oz -o optimized_for_size.wasm libtraffic_editor_iii_bg.wasm
+mv optimized_for_size.wasm libtraffic_editor_iii_bg.wasm
